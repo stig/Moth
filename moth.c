409e4eb5220549dfca09198ad5f8359495c7bc7b
stig <stig@b3023d8d-9cf6-0310-973e-fe5e02187dc7>
2005-05-09 16:39:21 +0000

    Revision: moth--devo--1.0--patch-21 Archive: stig@brautaset.org--arwen-03 Creator: Stig Brautaset <stig@brautaset.org> Date: Mon Mar 17 15:20:03 GMT 2003 Standard-date: 2003-03-17 15:20:03 GMT Summary: fix copyright notice Keywords: New-files: {arch}/moth/moth--devo/moth--devo--1.0/stig@brautaset.org--arwen-03/patch-log/patch-21 Modified-files: .arch-ids/Makefile.in.id .arch-ids/moth.c.id Makefile.in   PLUGIN/.arch-ids/=id PLUGIN/.arch-ids/REQ.id PLUGIN/REQ moth.c   {arch}/=tagging-method   {arch}/moth/moth--devo/moth--devo--1.0/stig@brautaset.org--arwen-03/patch-log/base-0   {arch}/moth/moth--devo/moth--devo--1.0/stig@brautaset.org--arwen-03/patch-log/patch-1   {arch}/moth/moth--devo/moth--devo--1.0/stig@brautaset.org--arwen-03/patch-log/patch-2   {arch}/moth/moth--main/moth--main--1.0/stig@brautaset.org--arwen-03/patch-log/base-0   {arch}/moth/moth--main/moth--main--1.0/stig@brautaset.org--arwen-03/patch-log/patch-1   {arch}/moth/moth--main/moth--main--1.0/stig@brautaset.org--arwen-03/patch-log/patch-2   {arch}/moth/moth--main/moth--main--1.0/stig@brautaset.org--arwen-03/patch-log/patch-3   {arch}/moth/moth--main/moth--main--1.0/stig@brautaset.org--arwen-03/patch-log/patch-4   {arch}/moth/moth--main/moth--main--1.0/stig@brautaset.org--arwen-03/patch-log/patch-5   {arch}/moth/moth--main/moth--main--1.0/stig@brautaset.org--arwen-03/patch-log/patch-6   {arch}/moth/moth--main/moth--main--1.0/stig@brautaset.org--arwen-03/patch-log/patch-7   {arch}/moth/moth--main/moth--main--1.0/stig@brautaset.org--arwen-03/patch-log/patch-8 New-patches: stig@brautaset.org--arwen-03/moth--devo--1.0--patch-21
    fix copyright notice

--- moth.c
+++ moth.c
@@ -2,9 +2,22 @@
  * moth -- my/mini Othello
  * Copyright (C) 2003 Stig Brautaset
  *
- * This software is released under the GNU GPL.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <time.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -77,6 +90,7 @@ int main(int argc, char **argv)
 		ply2 = atoi(argv[2]);
 	}
 
+	srand(time(NULL));
 	mainloop(game, ply1, ply2);
 
 	return 0;
@@ -165,7 +179,7 @@ void mainloop(struct ggtl *game, int ply1, int ply2)
 			board = ggtl_make_move(game, move);
 			
 			if (!board) {
-				board = ggtl_alphabeta_iterative(game, maxply);
+				board = ggtl_alphabeta(game, maxply);
 			}
 		}
 	} 
